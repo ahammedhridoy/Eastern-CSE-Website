d6d15d7d5c8c0c38dc1ec7b38e906124bdeb30e2
ahammedhridoy <developerhridoy@gmail.com>
2024-12-29 00:00:52 +0600

    Axios fix

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    "Access-Control-Allow-Origin":
+      "https://eastern-cse-website-frontend.vercel.app",
+  },
 });
 
 export default apiClient;
