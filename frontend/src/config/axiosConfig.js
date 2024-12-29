d045417f9695f18c556c2d2172e7977d4c274e5e
ahammedhridoy <developerhridoy@gmail.com>
2024-12-29 22:15:41 +0600

    cors error

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  headers: {
-    "Content-Type": "application/json", // Set content type if needed
-  },
-  withCredentials: true, // Include credentials (cookies, authorization headers) if needed
 });
 
 export default apiClient;
